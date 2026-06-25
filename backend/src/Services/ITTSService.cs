33cd860f154ec08f289e5369cf73448ac3b2a75d
MinhQuan <minhquanygo@gmail.com>
2026-06-26 00:45:49 +0700

    xoa backend

--- backend/src/Services/ITTSService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace src.Services
-{
-    public interface ITTSService
-    {
-        Task<string> GenerateAudioAsync(string text, string languageCode, int poiId);
-    }
-}
